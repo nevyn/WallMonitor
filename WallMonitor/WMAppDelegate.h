6a44545d99ca4c03d14dec16d81fdb3f4e61f50f
Joachim Bengtsson <joachimb@gmail.com>
2012-12-08 17:46:31 +0100

    Initial Commit

---
+++ WallMonitor/WMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WMAppDelegate.h
+//  WallMonitor
+//
+//  Created by Joachim Bengtsson on 2012-12-08.
+//  Copyright (c) 2012 Joachim Bengtsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WallMonitor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WallMonitor
+//
+//  Created by Joachim Bengtsson on 2012-12-08.
+//  Copyright (c) 2012 Joachim Bengtsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WMAppDelegate class]));
+    }
+}
